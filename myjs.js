f4a70cc9daec88ff284253e7bfc9a53368150f3b
Amankesarwani2202 <64828784+Amankesarwani2202@users.noreply.github.com>
2020-12-20 12:17:14 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,115 @@
+function one(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="1";
+    else
+        document.getElementById("t1").value=z+"1";
+    }
+    function two(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="2";
+    else
+        document.getElementById("t1").value=z+"2";
+    }
+    function three(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="3";
+    else
+        document.getElementById("t1").value=z+"3";
+    }
+    function four(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="4";
+    else
+        document.getElementById("t1").value=z+"4";
+    }
+    function five(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="5";
+    else
+        document.getElementById("t1").value=z+"5";
+    }
+    function six(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="6";
+    else
+        document.getElementById("t1").value=z+"6";
+    }
+    function seven(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="7";
+    else
+        document.getElementById("t1").value=z+"7";
+    }
+    function eight(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="8";
+    else
+        document.getElementById("t1").value=z+"8";
+    }
+    function nine(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="9";
+    else
+        document.getElementById("t1").value=z+"9";
+    }
+    function zero(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="0";
+    else
+        document.getElementById("t1").value=z+"0";
+    }
+    function decimal(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value=".";
+    else
+        document.getElementById("t1").value=z+".";
+    }
+    function sum(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="+";
+    else
+        document.getElementById("t1").value=z+"+";
+    }
+    function diff(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="-";
+    else
+        document.getElementById("t1").value=z+"-";
+    }
+    function multi(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="*";
+    else
+        document.getElementById("t1").value=z+"*";
+    }
+    function div(){
+    var z = document.getElementById("t1").value;
+    if(z=="" || z==null)
+        document.getElementById("t1").value="/";
+    else
+        document.getElementById("t1").value=z+"/";
+    }
+    function equal(){
+    var z = document.getElementById("t1").value;
+    
+        document.getElementById("t1").value=eval(z);
+    }
+    function clear1(){
+    
+        document.getElementById("t1").value="";
+        
+    }
